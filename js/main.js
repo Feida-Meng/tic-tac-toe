52d746c25205f5e83a8f0c1c80f71c1be3c38b25
codonculous <feidameng01@gmail.com>
2017-04-30 14:39:00 -0400

    make it recognize the winner when all three x/o are on the same horizontal line

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 $(function(){
   var o = 'O';
   var x = 'X';
-  var moves =[];
   var numX = 0;
   var numO = 0;
+  var firstRow  = $('.first-row h1');
+  var secondRow = $('.second-row h1');
+  var thirdRow = $('.third-row h1');
   // var start = ox[Math.floor((Math.random()))];
 
   $('.board').on('click', '.user-input', function() {
@@ -12,22 +14,33 @@ $(function(){
     if ( $(this).text() !== x && $(this).text() !== o ) {
       if ( numX > numO ) {
         $(this).text(o);
-        // moves.push(o);
         numO ++;
+        winner();
 
       } else {
         $(this).text(x);
         numX ++;
-        // moves.push(x);
+        winner();
       }
     }
 
   });
 
-  // function winer() {
-  //
-  //   // moves[];
-  // }
+  function winner() {
+
+    for (var a = 0;a < 3;a++) {
+      for (var b =0;b < 3;b++) {
+        for (var c = 0;c < 3;c++) {
+
+          if ( $(firstRow[a]).text() === $(firstRow[a+1]).text() && $(firstRow[a+1]).text() === $(firstRow[a+2]).text() ) {
+            $('.winner').text('Winner is: '+ $(firstRow[a]).text());
+          }
+
+
+        }
+      }
+    }
+  }
 
 });
 
