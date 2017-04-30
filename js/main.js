4ff2a6f7dea57533d55961bcbabf17c1bc0288c9
codonculous <feidameng01@gmail.com>
2017-04-30 14:14:08 -0400

    create javascript to allow user to make the proper move,ie.x or o

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 
+$(function(){
+  var o = 'O';
+  var x = 'X';
+  var moves =[];
+  var numX = 0;
+  var numO = 0;
+  // var start = ox[Math.floor((Math.random()))];
+
+  $('.board').on('click', '.user-input', function() {
+
+    if ( $(this).text() !== x && $(this).text() !== o ) {
+      if ( numX > numO ) {
+        $(this).text(o);
+        // moves.push(o);
+        numO ++;
+
+      } else {
+        $(this).text(x);
+        numX ++;
+        // moves.push(x);
+      }
+    }
+
+  });
+
+  // function winer() {
+  //
+  //   // moves[];
+  // }
+
+});
+
+//1st f[0] f[1] f[2]
+//2nd s[0] s[1] s[2]
+//3rd t[0] t[1] t[2]
